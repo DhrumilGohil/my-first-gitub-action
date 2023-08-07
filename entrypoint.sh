62ff667713d3ed6fb2b2910f0bbed12b944789e6
Dhrumil Gohil <57474739+DhrumilGohil@users.noreply.github.com>
2023-08-07 19:18:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,34 +7,34 @@ ssh-keyscan -H "$HOST_NAME" >> /root/.ssh/known_hosts
 
 
 #rsync -avz . $HOST_USER@$HOST_NAME:~/test
-rsync -avz --exclude .git --exclude .github . $HOST_USER@$HOST_NAME:/opt/sites/easyengine/ee-laravel.com/app/htdocs
+rsync -avz --exclude .git --exclude .github . $HOST_USER@$HOST_NAME:/opt/easyengine/sites/ee-laravel.com/app/htdocs
 
-# ssh -v $HOST_USER@$HOST_NAME '
-#             cd /opt/easyengine/sites/ee-laravel.com/app/htdocs
-#             ls
-#             mv public custom_public
-#             composer install
-#             # Create .env file (assuming you have all necessary environment variables set as secrets in GitHub)
-#             echo "APP_ENV=production" >> .env
-#             echo "APP_NAME=Laravel" >> .env
-#             echo "APP_DEBUG=false" >> .env
-#             echo "DB_CONNECTION=$DB_CONNECTION" >> .env
-#             echo "DB_HOST=$DB_HOST" >> .env
-#             echo "DB_PORT=$DB_PORT" >> .env
-#             echo "DB_DATABASE=$DB_DATABASE" >> .env
-#             echo "DB_USERNAME=$DB_USERNAME" >> .env
-#             echo "DB_PASSWORD=$DB_PASSWORD" >> .env
-#             echo "APP_KEY=" >> .env
-#             php artisan migrate:fresh — seed
-#             chmod -R o+w storage/ bootstrap/ 
-#             php artisan key:generate
-#             php artisan cache:clear
-#             php artisan config:clear
-#             php artisan route:clear
-#             php artisan view:clear
-#             php artisan config:clear
-#             composer dump-autoload 
-#             '
+ssh -v $HOST_USER@$HOST_NAME '
+            cd /opt/easyengine/sites/ee-laravel.com/app/htdocs
+            ls
+            mv public custom_public
+            composer install
+            # Create .env file (assuming you have all necessary environment variables set as secrets in GitHub)
+            echo "APP_ENV=production" >> .env
+            echo "APP_NAME=Laravel" >> .env
+            echo "APP_DEBUG=false" >> .env
+            echo "DB_CONNECTION=$DB_CONNECTION" >> .env
+            echo "DB_HOST=$DB_HOST" >> .env
+            echo "DB_PORT=$DB_PORT" >> .env
+            echo "DB_DATABASE=$DB_DATABASE" >> .env
+            echo "DB_USERNAME=$DB_USERNAME" >> .env
+            echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+            echo "APP_KEY=" >> .env
+            php artisan migrate:fresh — seed
+            chmod -R o+w storage/ bootstrap/ 
+            php artisan key:generate
+            php artisan cache:clear
+            php artisan config:clear
+            php artisan route:clear
+            php artisan view:clear
+            php artisan config:clear
+            composer dump-autoload 
+            '
 
 
 
