e45b3b54f759df051fa403599e815cf9bde97d15
Dhrumil Gohil <57474739+DhrumilGohil@users.noreply.github.com>
2023-08-07 11:49:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 
 echo "$SSH_PRIVATE_KEY"
-mkdir -p ~/.ssh 
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+mkdir -p .ssh 
+echo "$SSH_PRIVATE_KEY" > .ssh/id_rsa
+chmod 600 .ssh/id_rsa
 # ssh-keyscan $HOST_NAME >> ~/.ssh/known_hosts
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIRjsDf3oWrdcAJjg9e74PO6gOt7jrE2PFmOgCKRh2q" >> ~/.ssh/known_hosts
-cat ~/.ssh/known_hosts
-cat  ~/.ssh/id_rsa
-ls ~/ -a
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIRjsDf3oWrdcAJjg9e74PO6gOt7jrE2PFmOgCKRh2q" >> .ssh/known_hosts
+cat .ssh/known_hosts
+cat  .ssh/id_rsa
+ls -a
 pwd
 
 rsync -avz * $HOST_USER@$HOST_NAME:~/test
