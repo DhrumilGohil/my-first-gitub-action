99225a18781292ac91935d8a8d4fcaeb61f356c5
Dhrumil Gohil <57474739+DhrumilGohil@users.noreply.github.com>
2023-08-07 12:12:25 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 echo "$SSH_PRIVATE_KEY"
 mkdir -p ~/.ssh 
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600~/ .ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 # ssh-keyscan $HOST_NAME >> ~/.ssh/known_hosts
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIRjsDf3oWrdcAJjg9e74PO6gOt7jrE2PFmOgCKRh2q" >> ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
